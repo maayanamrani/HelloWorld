7e118ca4adfacbce6aa4c20df57ef11696f96ad8
maayanamrani <33892512+maayanamrani@users.noreply.github.com>
2019-03-03 14:34:30 +0200

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,8 +1,11 @@
 
-public class HelloWorld {
+package artifactory.test;
 
+/**
+ * Hello world!
+ */
+public class Multi1 {
     public static void main(String[] args) {
-        System.out.println("Hello!");
-
+        System.out.println("Hello World!");
     }
 }
